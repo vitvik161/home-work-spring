621ddbbbd579dbcf92bfd28acb595aeca52b3951
vitvik161 <88671741+vitvik161@users.noreply.github.com>
2021-10-04 17:19:17 +0300

    lesson 16

--- src/main/java/org/example/ConfigLesson16.java
+++ src/main/java/org/example/ConfigLesson16.java
@@ -14,18 +14,18 @@ import java.util.List;
 public class ConfigLesson16 {
     @Bean
     public Account account(){
-        return new Account(1,"RUB", LocalDate.of(2021, 1, 1));
+        return new Account(1,"RUB", LocalDate.of(2022, 1, 1));
     }
 
     @Bean
     public Account account01(){
-        return new Account(2,"RUB", LocalDate.of(2021, 2, 2));
+        return new Account(2,"RUB", LocalDate.of(2022, 2, 2));
     }
 
     @Bean
     public Client client(){
         List<Account> accountList = new ArrayList<Account>(Arrays.asList(account(), account01()));
-        return new Client((short) 18,"Ivan", accountList);
+        return new Client((short) 28,"Petr", accountList);
     }
 
 }
